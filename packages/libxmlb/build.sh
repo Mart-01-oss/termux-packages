8fa91512399edd38700bcb4b2fae07af1743723a
Mart-01-oss <remartrobles09@gmail.com>
2026-01-20 20:25:14 +0800

    fix

--- packages/libxmlb/build.sh
+++ packages/libxmlb/build.sh
@@ -7,7 +7,10 @@ TERMUX_PKG_SRCURL=https://github.com/hughsie/libxmlb/releases/download/${TERMUX_
 TERMUX_PKG_SHA256=ded52667aac942bb1ff4d1e977e8274a9432d99033d86918feb82ade82b8e001
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="glib, liblzma, libstemmer, zstd"
-TERMUX_PKG_BUILD_DEPENDS="g-ir-scanner, glib-cross"
+# libxmlb enables GObject Introspection in TERMUX_PKG_EXTRA_CONFIGURE_ARGS,
+# so we need the gobject-introspection pkg-config files available at build time.
+# Meson may also try CMake as a fallback for some deps.
+TERMUX_PKG_BUILD_DEPENDS="cmake, g-ir-scanner, glib-cross, gobject-introspection"
 TERMUX_PKG_VERSIONED_GIR=false
 TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
